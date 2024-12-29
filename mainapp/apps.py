2842f643040810db553dec9559012e3af8a22614
ohyoxo <126952400+ohyoxo@users.noreply.github.com>
2024-12-29 22:42:23 +0800

    Update apps.py

--- mainapp/apps.py
+++ mainapp/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 class MainappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mainapp'
+
+    def ready(self):
+        from . import utils
+        utils.start_server()
